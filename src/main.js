5ebd87bd527234ad3f909037ecadecc18981f50e
abhiit89 <abhi.aec89@gmail.com>
2017-11-27 21:30:33 +0530

    Remove some logs

--- src/main.js
+++ src/main.js
@@ -8,8 +8,6 @@ angCoin.addNewTransactionBlockToTransactionChain(new Block(uuid(), new Date().ge
 console.log('Is BlockChain Valid after first Block? ' + angCoin.isBlockChainValid());
 angCoin.addNewTransactionBlockToTransactionChain(new Block(uuid(), new Date().getTime().valueOf(), {amount : '10$'}));
 console.log('Is BlockChain Valid after second Block? ' + angCoin.isBlockChainValid());
-console.log('Blockchain so far ');
-// console.log(JSON.stringify(angCoin, null, 4));
 console.log('Tampering the Data of the First Block (not Genesis Block)');
 angCoin.transactionChain[1].transactionData = {amount : '$100'};
 console.log('Is BlockChain Valid after Tampering? ' + angCoin.isBlockChainValid());
